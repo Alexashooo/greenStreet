650de2d1c50492badc374808d7989afd8f03bd57
Alexashooo <aleksandar.sunjic@gmail.com>
2016-12-30 23:31:44 +0100

    Resizible working, not finished

--- app/assets/javascripts/ng-app/directives/streetProfile.js
+++ app/assets/javascripts/ng-app/directives/streetProfile.js
@@ -7,11 +7,27 @@
               restrict: 'E',
               scope: {},
               link: function(scope, element, attrs) {
+
+                   //write  custom resizible() function
+                  // use alsoResize to resize other elements
+                  //maxHeight: 80 can be one solution to limit vertical resizing to 0
+                  //maxWidth: ? can be one solution to limit horizontal resizing
+                  //minWidth
+                   element.children().resizable({
+                     handles: 'e, w',
+                     resize: function(event, ui){
+                         ui.element.css(ui.originalPosition);
+                     }
+                   });
+
                    element.disableSelection().sortable({
+                     tolerance: 'pointer',
                      scroll: true,
                      placeholder: 'placeholder',
+                     containment: 'parent',
                      axis: 'x'
-                    }).droppable({
+
+                     }).droppable({
                       drop: function(event, ui) {
                         //prevent profile items to change while drag/drop
                         if($(ui.draggable).hasClass('dirtyitem')){
@@ -21,12 +37,19 @@
                            $(ui.draggable).addClass('item ui-sortable-handle');
                             //adjusting height and width
                            $(ui.draggable).height($('.item').css('height'));
-                           $(ui.draggable).width($('.item').css('width'));
-
+                           $(ui.draggable).width(100);
+                           $(ui.draggable).resizable({
+                             handles: 'e, w',
+                             resize: function(event, ui){
+                                 ui.element.css(ui.originalPosition);
+                             }
+                           });
 
                            //Adding HTML for arrows
-                          $(ui.draggable).append("<div class='resize-arrow-left'></div>");
-                          $(ui.draggable).append("<div class='resize-arrow-right'></div>");
+                          $(ui.draggable).append(
+                               "<div class='resize-arrow-left'></div>",
+                               "<div class='resize-arrow-right'></div>"
+                          );
 
                         }
                       }
